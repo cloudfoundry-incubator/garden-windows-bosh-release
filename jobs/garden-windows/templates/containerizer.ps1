0b2f564c489d8d7af245b84dfc1cbf91874cb19d
Amin Jamali <ajamali@pivotal.io>
2016-05-26 14:33:36 -0400

    Containerizer IP is no longer hard coded
    [#120278631]

--- jobs/garden-windows/templates/containerizer.ps1
+++ jobs/garden-windows/templates/containerizer.ps1
@@ -1,1 +1,1 @@
-C:\var\vcap\packages\garden-windows\Containerizer.exe --machineIp 10.10.5.68 --port 1788
+C:\var\vcap\packages\garden-windows\Containerizer.exe --machineIp <%= spec.networks.default.ip %> --port 1788

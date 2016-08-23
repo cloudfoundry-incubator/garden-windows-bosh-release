6c778cfb635a3c923e8a06978c8b6618f9d71c59
Pivotal <cf-netgarden-eng@pivotal.io>
2016-08-23 09:43:25 -0400

    Revert "Add a post-start job that restarts metron_agent"
    This reverts commit 36919567f8bfb2a02c6d2cf0073e78687b002a95.

--- jobs/garden-windows/templates/post-start.ps1
+++
@@ -1,4 +0,0 @@
-# Work around for issue https://github.com/cloudfoundry/loggregator/issues/146
-# This restarts metron_agent in case it failed to connect to etcd
-
-Restart-Service -Name "metron_agent" -Force
